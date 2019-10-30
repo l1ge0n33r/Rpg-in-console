deb6bbaa3f14181d51e060938e23d5f7eb080d48
l1ge0n33r <48294455+l1ge0n33r@users.noreply.github.com>
2019-10-30 19:42:00 +0200

    Add files via upload

--- RPG in console (test).cpp
+++ RPG in console (test).cpp
@@ -3,14 +3,14 @@
 //
 //
 
-
+#include "pch.h"
 #include <iostream>
+#include <fstream>
 #include <string>
-using namespace std;
 
+; using namespace std;
 /*TODO: 
--главное меню
--систему сохранить-загрузить
+-систему сохранить-загрузить (IN PRIORITY!!!)
 
 -добавить арену 
 -добавить врагов
@@ -23,7 +23,7 @@ using namespace std;
 
 -вход\выход из здания
 */
-
+string nickName;
 bool gameover = false;
 const short int spawn_id = 1;
 const short  int Msquare_id = 2;
@@ -33,100 +33,87 @@ string enetre;
 const short int shop_id = 10001;
 const short int info_id = 10002;
 short int choisesss;
+bool mainMenu = true;
 
-void mvn_shop() {
-	cout << "You are at the shop \n";
-	cout << "Choise action:\n";
-	cout << "1.Buy\n";
-	cout << "2.Sell\n";
-	cout << "0.Leave the store\n";
-	cin >> choisesss;
-	if (choisesss == 1) 
+void Save_Load();
+
+
+
+void shopActions();
+
+//moving from main square
+void mvn_mainsqv();
+
+
+//moving from spawnpoint
+void mvnSpawn();
+
+void Choises();
+
+void MainMenu();
+
+int main()
+{
+	while (!gameover) 
 	{
-		cout << "This action is not availble yet.";
-		mvn_shop();
+		MainMenu();	
 	}
-	else if (choisesss == 2)
+	return 0;
+}
+
+
+
+void MainMenu()
+{
+	cout << "Start game" << endl;
+	cout << "Load game" << endl;
+	cout << "Quit" << endl;
+	cin >> enetre;
+	if (enetre == "Start game" || enetre == "start" || enetre == "Start")
 	{
-		cout << "This action is not availble yet.";
-		mvn_shop();
-	}
-	if (choisesss == 0)
-	{
-		location = Msquare_id;
 		
+		// nickname input
+
+		// diffuculty (usless)
+
+		//
+		mainMenu = false;
+
+		while (!gameover && mainMenu == false)
+		{
+			Choises();
+		}
+		if (mainMenu == false) {
+			cout << "GameOver" << endl;
+		}
 	}
+	else if (enetre == "Load game")
+	{
+
+	}
+	else if (enetre == "Quit" || enetre == "quit")
+	{
+		gameover = true;
+		mainMenu = true;
+	}
+
+	// начать игру
+
+	//сохранит - загрузить
+
+	//выход
+
+
+
+	system("cls");
 }
 
-void mvn_mainsqv() {
-	cout << "You are at the Main square'" << endl;
-	cout << "Choise your destination: " << endl;
-	cout << "1.Spawn" << endl;
-	cout << "2.Store" << endl;
-	cout << "3.Arena" << endl;
-	cin >> destiny;
-
-	if (destiny == "1" || destiny == "spawn" || destiny == "Spawn"|| destiny == "1.Spawn")
-	{
-		location = spawn_id;
-		system("cls");
-	}
-	else if (destiny == "2") 
-	{
-		location = shop_id;
-	}
-	else if (destiny == "0") 
-	{}
-	else {
-		cout << "This choise isn't avaible yet. Or there is no destiny under id " + destiny << endl;
-		system("pause");
-		system("cls");
-
-
-		mvn_mainsqv();
-
-
-	}
-
-}
-
-
-
-// передвижения со спавна
-void mvnSpawn() {
-	cout << "You are at the spawn" << endl;
-	cout << "Choise your destination: " << endl;
-	cout << "1.Main_square" << endl;
-	cout << "0.Go back" << endl;
-	cin >> destiny;
-	 
-	
-	if (destiny == "1")
-	{
-		location = Msquare_id; 
-		system("cls");
-	}
-	else if (destiny == "0") {
-		
-	}
-	else
-	{
-		cout << "There is no destiny under id " + destiny << endl;
-		system("pause");
-		system("cls");
-
-
-		mvnSpawn();
-
-
-	}
-
-}
 
 void Choises() {
 	system("cls");
 
 	cout << "Type 'move' to choise, where you'd like to move. Type 'lol'," << endl << " to get smile. Type'quit' to quit the game." << endl << "If you need help type 'help'." << endl;
+	cout << "Type 'Menu' to get to  the Main menu" << endl;
 	cin >> enetre;
 
 	if (enetre == "move") //выбор передвижения
@@ -147,24 +134,37 @@ void Choises() {
 	}
 	else if (enetre == "quit")  //выход из игры
 	{
+		mainMenu = false;
 		gameover = true;
 	}
 	else if (enetre == "O_mae_wa_mou_shinderu") { //отсылка
 		cout << "NANI!!!???? \n";
-	
+
 		system("pause");
-		gameover = true; 
+		gameover = true;
+
 	}
-	else if (enetre == "determination") 
+	else if (enetre == "determination")
 	{
 		cout << "Life is a river so many ups downs, \n twist and turns, some points are clear and smooth, \n others still are as white water rapids, hazy and rough, \n and we are but a Lilly along for the ride, \n hold on and fallow the flow we must, \n we may try to cling to certain parts but we will \n only be swept away in the current of time  \n (c) Eponia's Will Klock \n";
 		system("pause");
 	}
-	else if (enetre == "help") 
+	else if (enetre == "help")
 	{
-		cout << "To chose where you'd like to move, you should type 'move' and than id of destinatoin from the list"<<endl;
+		cout << "To chose where you'd like to move, you should type 'move' and than id of destinatoin from the list" << endl;
 		system("pause");
 	}
+	else if (enetre == "save")
+	{
+		cout << "not yet =) \n";
+		system("pause");
+	}
+
+	else if (enetre == "Menu")
+	{
+		mainMenu = true;
+	}
+
 	else {
 		cout << "You entered wrong action. Please try again";
 		system("pause");
@@ -173,14 +173,99 @@ void Choises() {
 
 }
 
+void mvnSpawn() {
+	cout << "You are at the spawn" << endl;
+	cout << "Choise your destination: " << endl;
+	cout << "1.Main_square" << endl;
+	cout << "0.Go back" << endl;
+	cin >> destiny;
 
 
-int main()
-{
-	while(!gameover){
-	Choises();
+	if (destiny == "1")
+	{
+		location = Msquare_id;
+		system("cls");
+	}
+	else if (destiny == "0") {
+
+	}
+	else
+	{
+		cout << "There is no destiny under id " + destiny << endl;
+		system("pause");
+		system("cls");
+
+
+		mvnSpawn();
+
+
+	}
+
 }
-	cout << "GameOver"<<endl;
-	system("pause");
-	return 0;
+
+void mvn_mainsqv() {
+	cout << "You are at the Main square'" << endl;
+	cout << "Choise your destination: " << endl;
+	cout << "1.Spawn" << endl;
+	cout << "2.Store" << endl;
+	cout << "3.Arena" << endl;
+	cin >> destiny;
+
+	if (destiny == "1" || destiny == "spawn" || destiny == "Spawn" || destiny == "1.Spawn")
+	{
+		location = spawn_id;
+		system("cls");
+	}
+	else if (destiny == "2")
+	{
+		location = shop_id;
+	}
+	else if (destiny == "0")
+	{
+	}
+	else {
+		cout << "This choise isn't avaible yet. Or there is no destiny under id " + destiny << endl;
+		system("pause");
+		system("cls");
+
+
+		mvn_mainsqv();
+
+
+	}
+
+}
+
+void shopActions() {
+	cout << "You are at the shop \n";
+	cout << "Choise action:\n";
+	cout << "1.Buy\n";
+	cout << "2.Sell\n";
+	cout << "0.Leave the store\n";
+	cin >> choisesss;
+	if (choisesss == 1)
+	{
+		cout << "This action is not availble yet.";
+		shopActions();
+	}
+	else if (choisesss == 2)
+	{
+		cout << "This action is not availble yet.";
+		shopActions();
+	}
+	if (choisesss == 0)
+	{
+		location = Msquare_id;
+
+	}
+}
+
+
+void Save_Load()
+{
+	//
+	//	ник
+	//	статы
+	//	инвентарь
+	//
 }
